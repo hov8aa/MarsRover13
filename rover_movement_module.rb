9c7b61e2ca5c7cbee99aab20338a00e2f5b899db
hov8aa <hov8aa@gmail.com>
2024-10-12 01:13:56 +0530

    added the turn_rover_left function to match with the context in specs

--- rover_movement_module.rb
+++ rover_movement_module.rb
@@ -2,3 +2,14 @@ def move_rover(rover_face_direction)
     return 'W'
 end
 
+def turn_rover_left(rover_face_direction)
+    if rover_face_direction == 'N'
+        return 'W'
+        elsif rover_face_direction == 'W'
+            return 'S'
+            elsif rover_face_direction == 'S'
+                return 'E'
+                elsif rover_face_direction == 'E'
+                    return 'N'
+                end
+end
